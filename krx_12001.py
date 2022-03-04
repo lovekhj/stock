87679f5a284ba64da48e6b9a437aad4e89944b83
jong <lovekhj@gmail.com>
2022-03-04 22:56:39 +0900

    20220304

--- krx_12001.py
+++ krx_12001.py
@@ -9,8 +9,9 @@ def krx_price(tdate):
 
     gen_req_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
     query_str_parms = {
+        'locale': 'ko_KR',
         'mktid': 'All',
-        'trdDd': '20220302',
+        'trdDd': '20220304',
         'share': '1',
         'money': '1',
         'csvxls_isNo': 'false',
@@ -18,7 +19,9 @@ def krx_price(tdate):
         'url': 'dbms/MDC/STAT/standard/MDCSTAT01501'
     }
     headers = {
-        'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020101'}
+        'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020101',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
+    }
     otp = requests.post(gen_req_url, query_str_parms, headers=headers)
 
     time.sleep(1.0)
@@ -29,13 +32,18 @@ def krx_price(tdate):
     r = requests.post(down_url, {'code': otp}, headers=headers)
     time.sleep(1.0)
     try:
-        df = pd.read_excel(BytesIO(r.content), thousands=',',
-                           converters={'종목코드': str})
-        df.to_excel('krx_0302.xlsx', index=False,
-                    index_label=None, encoding='utf-8-sig')
+        down_ks = pd.read_csv(BytesIO(r.content), encoding='EUC-KR')
+        print(down_ks)
+        # df = pd.read_excel(BytesIO(r.content), thousands=','
+        #                    converters={'종목코드': str})
+        # df.to_excel('krx_0302.xlsx', index=False,
+        #             index_label=None, encoding='utf-8-sig')
     except:
         print("파일이 손상되었습니다.")
         return ""
 
 
-krx_price('20220302')
+# -------------------------------------------------------
+# krx : 12001 전종목시세 다운로드
+# -------------------------------------------------------
+krx_price('20220304')
