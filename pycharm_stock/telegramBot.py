a4e6f10b1a5ff65c27b6d2910eb0d58588ce863d
lovekhj <lovekhj@gmail.com>
2022-04-23 15:37:27 +0900

    telegrambot - 0423

---
+++ pycharm_stock/telegramBot.py
@@ -0,0 +1,11 @@
+# pip install python-telgram-bot --upgrade
+# conda install -c conda-forge python-telegram-bot
+
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+
+
+print('telegram bot')
