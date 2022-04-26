17ed3f31fbd7ae28f39e2bb9c85f5fa57bf11e48
lovekhj <lovekhj@gmail.com>
2022-04-26 17:58:56 +0900

    realtime stock

--- pycharm_stock/getRealTimeStock.py
+++ pycharm_stock/getRealTimeStock.py
@@ -2,18 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 import interestItem as iITEM
+import telegramBot as tb
 
 
 # print(iITEM.interstItemList)
 
+stock_id = '005930'
+stock_nm = '삼성전자'
+
+
 # 삼성전자
-url = 'https://finance.naver.com/item/main.naver?code=005930'
+url = 'https://finance.naver.com/item/main.naver?code={}'.format(stock_id)
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"}
 res = requests.get(url, headers=headers)
 
 # 주식종목, 현재주가, 전일가, 전일대비 금액, 상승/하락%
 soup = BeautifulSoup(res.text, 'lxml')
-stock_price = soup.find('div', attrs={'class','today'}).find('span','blind')
-stock_price_yesterday = soup.find('div', attrs={'class','today'})
-# print(stock_price_yesterday)
+stock_price = soup.find('div', attrs={'class','today'}).find('span','blind').text
+stock_price_yesterday = soup.find('div',attrs={'class','rate_info'}).find('td', attrs={'class','first'}).find('span','blind').text
+stock_price_chai = soup.find('div', attrs={'class','today'}).find_all('em',attrs={'class','no_down'})[1].find('span','blind').text
+stock_price_chai_rate = soup.find('div', attrs={'class','today'}).find_all('em',attrs={'class','no_down'})[2].find('span','blind').text +\
+                        soup.find('div', attrs={'class','today'}).find_all('em',attrs={'class','no_down'})[2].find('span','per').text
+
+stock_all = "주식종목 : {}:{} 현재가:{}, 전일가:{}, 전일대비:{}({})".format(stock_id, stock_nm, stock_price \
+                                                              , stock_price_yesterday
+                                                              , stock_price_chai
+                                                              , stock_price_chai_rate)
+print(stock_all )
+tb.send_telegram_bot(stock_all)
+
--- pycharm_stock/telegramBot.py
+++ pycharm_stock/telegramBot.py
@@ -4,18 +4,25 @@
 import telegram
 import jackToken as tToken
 
-my_token = tToken.telegram_my_token
-bot = telegram.Bot(token=my_token)  # bot 선언
-updates = bot.getUpdates()  # update 내역을 받아옴
-print(updates)
+def send_telegram_bot(send_msg):
+    print('telegrambot : ' , send_msg)
 
-# for u in updates:
-#     print(u.message)
+    my_token = tToken.telegram_my_token
+    bot = telegram.Bot(token=my_token)  # bot 선언
+    updates = bot.getUpdates()  # update 내역을 받아옴
+    print(updates)
 
-chat_id = bot.getUpdates()[-1].message.chat.id #가장 최근에 온 메세지의 chat id를 가져옵니다
-to_name = bot.getUpdates()[-1].message.chat.first_name
-print("char_id : {}, to_name : {}".format(chat_id, to_name))
-bot.sendMessage(chat_id = chat_id, text=to_name+"님  저는 lovekhj_bot 입니다.")
+    # for u in updates:
+    #     print(u.message)
 
-print('telegram bot')
+    chat_id = bot.getUpdates()[-1].message.chat.id #가장 최근에 온 메세지의 chat id를 가져옵니다
+    to_name = bot.getUpdates()[-1].message.chat.first_name
+    print("char_id : {}, to_name : {}".format(chat_id, to_name))
+    # bot.sendMessage(chat_id = chat_id, text=to_name+"님  저는 lovekhj_bot 입니다.")
+    bot.sendMessage(chat_id = chat_id, text= send_msg)
+
+    print('telegram bot')
+
+if __name__ == "__main__":
+    send_telegram_bot(send_msg)
 
